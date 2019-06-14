177de280c1288d13321da293ef8bd75a3130d98b
Invitado <invitado@s5pc33.eisc.univalle.edu.co>
2019-06-14 12:21:42 -0500

    Adicionando fork-basico.c

--- 2019_06_14/fork-basico.c
+++ 2019_06_14/fork-basico.c
@@ -1,5 +1,7 @@
-#include<unistd.h>
-#include<stdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 /**
 Este programa en C debe imprimir lo siguiente:
 
@@ -16,9 +18,14 @@ int main(int argc, char** argv){
 	pid=fork();
 	int id=getpid();
 	if(pid==0){
+		sleep(3);
 		printf("Soy el hijo y mi identificador es %d \n",id);
 	}
 	if(pid>0){
+		int status;
+		pid_t pid2;
 		printf("Soy el padre, mi identificador es %d y el de mi hijo es %d \n",id,pid);
+		pid2=wait(NULL);
+		printf("terminó mi hijo %d \n",pid2);
 	}
 }
